71659c060dabcd2ab58849ea8397a805300f9c94
dbb12200 <dbb12200@users.noreply.github.com>
2017-02-14 21:27:59 -0500

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,4 +1,42 @@
+import java.awt.Image;
+import java.util.ArrayList;
 
 public class Game {
-
+	ArrayList<Doodad> doodadCol = new ArrayList<Doodad>();
+	
+	
+	
+	public String getBoardName(){
+		
+	}
+	
+	public Image fileToBoard(){
+		
+	}
+	
+	public Image displayBoard(){
+		
+	}
+	
+	public static void displayDoodads(){
+		
+	}
+	
+	public void addDoodad(Doodad d){
+		doodadCol.add(d);
+	}
+	
+	public void removeDoodad(Doodad d){
+		doodadCol.remove(d);
+	}
+	
+	public void readDoodad(){
+		
+	}
+	
+	public void updateCoords(){
+		
+	}
+	
+	
 }
